37e69167e535ee1ca499a7287262c9c5ae561c26
ozgursel <ozgurselr@gmail.com>
2018-01-16 18:27:55 +0300

    Payment Screen Update

--- src/app/components/store/payment-screen/payment-screen.component.ts
+++ src/app/components/store/payment-screen/payment-screen.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { MainService } from '../../../services/main.service';
 import { MessageService } from '../../../providers/message.service';
+import { Check } from '../../../mocks/check.mock';
 
 @Component({
   selector: 'app-payment-screen',
@@ -11,14 +12,36 @@ import { MessageService } from '../../../providers/message.service';
 })
 export class PaymentScreenComponent implements OnInit {
   id: string;
+  check: Check;
+  payedShow: boolean;
+  payedTitle: string;
+  numboard: Array<any>;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private mainService: MainService, private messageService: MessageService) {
+    this.numboard = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[".", 0, "✔"],];
+    this.payedShow = true;
+    this.payedTitle = 'Alınan Ödemeleri Gizle';
     this.route.params.subscribe(params => {
       this.id = params['id'];
+      this.mainService.getData('checks', this.id).then(res => {
+        console.log(res);
+        this.check = res;
+      })
     });
   }
 
   ngOnInit() {
   }
+
+  togglePayed() {
+    if (this.payedShow) {
+      this.payedShow = false;
+      this.payedTitle = 'Alınan Ödemeleri Görüntüle';
+    } else {
+      this.payedShow = true;
+      this.payedTitle = 'Alınan Ödemeleri Gizle';
+    }
+  }
+
 
 }
