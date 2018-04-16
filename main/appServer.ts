04982fda53174907ceb95616f5ac26451ab5a44b
ozgursel <ozgurselr@gmail.com>
2018-04-16 15:52:21 +0300

    AppServer InMemory Integration

--- main/appServer.ts
+++ main/appServer.ts
@@ -1,17 +1,21 @@
 import { ipcMain, webContents } from 'electron';
 import * as PouchDB from 'pouchdb';
+import * as InMemory from 'pouchdb-adapter-memory';
 import * as express from 'express';
 import * as expressPouch from 'express-pouchdb';
+
 let server
+PouchDB.plugin(InMemory);
+
 ipcMain.once('appServer', (event, token, port) => {
     const app = express();
-    app.use(`/${token}/`, expressPouch(PouchDB, { logPath: './data/log.txt', configPath: './data/config.json' }));
-    const appServer = new PouchDB('appServer');
+    app.use(`/${token}/`, expressPouch(PouchDB.defaults({ adapter: 'memory' }), { logPath: './data/log.txt', configPath: './data/config.json' }));
+    const appServer = new PouchDB('./data/appServer');
     server = app.listen(port);
 });
 
 ipcMain.once('closeServer', (event) => {
-    if(server !== undefined){
+    if (server !== undefined) {
         server.close();
     }
 });
